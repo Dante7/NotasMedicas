c2b15591dea2804b193f895232baa4aff43669bf
Dante7 <dante.arkade7@gmail.com>
2015-02-19 08:47:37 -0600

    Test 1 de responsive
    Usando bootstrap para diseño responsive

--- NotasMedicas/captura/forms.py
+++ NotasMedicas/captura/forms.py
@@ -1,13 +1,12 @@
 #encoding:utf-8
 
 from django.forms import ModelForm
-from django.forms.widgets import HiddenInput, RadioSelect
+from django.forms.widgets import HiddenInput, RadioSelect, DateInput
 from django.contrib.admin.widgets import AdminDateWidget
 from django import forms
 from captura.models import *
 
 notas = (
-			('',''),
 			('Ingreso','Ingreso'),
 			('Alta','Alta'),
 			('Gravedad','Gravedad'),
@@ -16,12 +15,24 @@ notas = (
 
 class FrmNota(ModelForm):
 	"""Formulario de llenado de notas"""
-	tipo_nota = forms.ChoiceField(label="",choices=notas, widget=forms.RadioSelect(attrs={'class':'form-control'}))
+	tipo_nota = forms.ChoiceField(choices=notas)
+	tipo_nota.widget.attrs['class']='form-control'
+
 	descripcion = forms.CharField(widget=forms.Textarea(attrs={'class':'form-control','rows':'3'}))
+
 	hospital = forms.CharField()
+	hospital.widget.attrs['class']='form-control'
+
 	medico = forms.CharField()
+	medico.widget.attrs['class']='form-control'
+
 	nss = forms.CharField()
+	nss.widget.attrs['class']='form-control'
+
 	cama = forms.CharField()
-	fecha_hora = forms.DateField()
+	cama.widget.attrs['class']='form-control'
+
+	fecha_hora = forms.DateField(widget=forms.DateInput(format=('%d-%m-%Y'),attrs={'class':'form-control','rows':'3'}))
+
 	class Meta:
 		model = Tbl1Nota
