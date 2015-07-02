f9157a13a3573e94b5d3a6535c4932cbd2528f85
Dante7 <dante.arkade7@gmail.com>
2015-07-02 03:25:16 -0500

    Dajax & Dajaxice complete
    Conf and test do it next step implentation :) :+1:

--- NotasMedicas/captura/ajax.py
+++ NotasMedicas/captura/ajax.py
@@ -1,7 +1,18 @@
 #encoding:utf-8
+
+from dajax.core import Dajax
 import json
 from dajaxice.decorators import dajaxice_register
 
+#Dajax functions
+@dajaxice_register
+def Concat(request, a, b):
+    dajax = Dajax()
+    result = a + b
+    dajax.assign('#result','value',str(result))
+    return dajax.json()
+
+#Dajaxice functions
 @dajaxice_register
 def sayhello(request):
     return json.dumps({'message':'Hello World'})
